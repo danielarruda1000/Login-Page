c578d1f43c5c2afc9f0c1daf48cf780a89645d59
Daniel Arruda <danielarruda1000>
2019-08-29 18:08:28 -0300

    Frontend

---
+++ view/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css'
+import RegisterForm from './components/RegisterForm'
+
+
+function App() {
+  return (
+    <div className="App">
+      <RegisterForm/>
+    </div>
+  );
+}
+
+export default App;
